b8dda8ae04c5f19457ab23f840e38b1dff9e0abb
= <uzzini@naver.com>
2020-12-28 17:55:10 +0900

    0.2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'joo',
+      password: '8377',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
+    RestaurantsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,15 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { createRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver(() => Restaurant)
+export class RestaurantResolver {
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+  @Mutation(() => Boolean)
+  createRestaurant(@Args() createRestaurantDto: createRestaurantDto): boolean {
+    return true;
+  }
+}
